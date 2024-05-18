cc5bb36cdcbac6551fc7ffce324f444a54a82420
deV1niborges <138510552+deV1niborges@users.noreply.github.com>
2024-05-18 15:46:42 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,62 @@
-// Nullish Coalescing Operator
+/* Nullish Coalescing Operator
 
-const idade = 0;
+const idade = 0;  // A variável idade é definida como 0
 
-document.body.innerText = "Sua idade é: " + (idade ?? "Não informado")
+// Utilizamos o operador de coalescência nula (??) para definir um valor padrão caso idade seja null ou undefined
+document.body.innerText = "Sua idade é: " + (idade ?? "Não informado");
+// Como idade é 0 (um valor válido), o resultado será "Sua idade é: 0"
 
 
-//Object
+// Object 
+
+const user = {
+  name: "Vini",  // Propriedade name do objeto user
+  nickname: "Gustavo",  // Propriedade nickname do objeto user
+  idade: 19,  // Propriedade idade do objeto user
+  address: {  // Propriedade address do objeto user, que é um objeto aninhado
+    street: "Rua Teste",  // Propriedade street do objeto address
+    number: 176,  // Propriedade number do objeto address
+  },
+};
+
+
+// Destructuring 
+
+const { address, idade: age, nickname = "Borges" } = user;
+// Desestruturamos o objeto user para extrair address, idade (renomeada para age) e nickname
+// Se nickname não estivesse definido, ele receberia o valor padrão "Borges"
+
+document.body.innerText = JSON.stringify({ address, age, nickname });
+// Convertemos o objeto com as propriedades extraídas em uma string JSON e a exibimos no corpo do documento
+
+
+// Rest Operator 
+
+const { name, idade, ...rest } = user;
+// Extraímos name e idade do objeto user, e o operador rest captura o restante das propriedades em um novo objeto rest
+
+const array = [1, 2, 3, 4, 5];
+
+const [first, , third, ...restArray] = array;
+// Extraímos o primeiro (first) e o terceiro (third) elementos do array
+// O operador rest captura o restante dos elementos em um novo array restArray
+
+document.body.innerText = JSON.stringify({ first, third, restArray });
+// Convertemos o objeto com as propriedades extraídas em uma string JSON e a exibimos no corpo do documento
+
+
+//Short Syntax 
+
+const name = "Vini";  // Definimos a variável name
+const age = 19;  // Definimos a variável age
+
+const user = {
+  name,  // Utilizamos a sintaxe curta para definir a propriedade name do objeto user
+  age,  // Utilizamos a sintaxe curta para definir a propriedade age do objeto user
+};
+
+document.body.innerText = JSON.stringify(user);
+// Convertemos o objeto user em uma string JSON e a exibimos no corpo do documento
+*/
+
+// Optional Chaining
